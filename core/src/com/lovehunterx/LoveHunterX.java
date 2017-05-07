a0178dca0bc83b7d757091995504f2fc11a93c2f
Hans Montero <hmontero1205@bths.edu>
2017-05-06 23:34:22 -0400

    added LHX Logo

--- core/src/com/lovehunterx/LoveHunterX.java
+++ core/src/com/lovehunterx/LoveHunterX.java
@@ -13,7 +13,7 @@ public class LoveHunterX extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("LoveHunterXIcon.png");
 	}
 
 	// hello there
@@ -21,10 +21,10 @@ public class LoveHunterX extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 3, 0, 1);
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 50, 50);
+		batch.draw(img, 100, 100,205,400);
 		batch.end();
 	}
 	
