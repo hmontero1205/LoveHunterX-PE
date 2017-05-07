22217f295168ce88b7911a22fcbd3cb6638d21fb
Hans Montero <hmontero1205@bths.edu>
2017-05-06 23:11:33 -0400

    PLS WORK

--- core/src/com/lovehunterx/LoveHunterX.java
+++ core/src/com/lovehunterx/LoveHunterX.java
@@ -18,7 +18,7 @@ public class LoveHunterX extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 3, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 50, 50);
