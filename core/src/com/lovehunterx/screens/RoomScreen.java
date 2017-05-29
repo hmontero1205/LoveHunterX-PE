857eaa90899d5a8dd24f0959d1c752624a7fbc76
Hans Montero <hmontero1205@bths.edu>
2017-05-28 22:39:44 -0400

    drag and drop/ toggle drag and drop WIP

--- core/src/com/lovehunterx/screens/RoomScreen.java
+++ core/src/com/lovehunterx/screens/RoomScreen.java
@@ -18,11 +18,15 @@ import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.DragListener;
+import com.badlogic.gdx.scenes.scene2d.utils.FocusListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.lovehunterx.Assets;
 import com.lovehunterx.LoveHunterX;
 import com.lovehunterx.game.entities.Player;
@@ -34,12 +38,14 @@ import com.lovehunterx.screens.ui.FixedGroup;
 import java.util.ArrayList;
 
 public class RoomScreen extends LHXScreen {
-    private Stage stage;
+    public Stage stage;
     private String room;
+    private ArrayList<Furniture> inventory;
+    private SideBar invBar;
 
     @Override
     public void show() {
-        stage = new Stage(new ExtendViewport(640, 480));
+        stage = new Stage(new StretchViewport(640, 480));
         Gdx.input.setInputProcessor(stage);
         Image back = new Image(new Texture(Gdx.files.internal("roomBack.jpg")));
         back.setPosition(centerX(back), centerY(back));
@@ -75,16 +81,17 @@ public class RoomScreen extends LHXScreen {
         fixed.addActor(right);
         stage.addActor(fixed);
 
-        SideBar invBar = new SideBar(0,65,65,350);
+        invBar = new SideBar(0,65,65,350);
         stage.addActor(invBar);
-        invBar.addItem("Love Sofa");
-        invBar.addItem("Love Sofa");
-        invBar.addItem("Love Sofa");
-        invBar.addItem("Love Sofa");
-        invBar.addItem("Love Sofa");
-        invBar.addItem("Love Sofa");
-        invBar.addItem("Love Sofa");
-        invBar.addItem("Love Sofa");
+        inventory = new ArrayList<Furniture>();
+        inventory.add(new Furniture(200,30, "Love Sofa"));
+        inventory.add(new Furniture(200,30, "Love Sofa"));
+        inventory.add(new Furniture(200,30, "Love Sofa"));
+        inventory.add(new Furniture(200,30, "Love Sofa"));
+        inventory.add(new Furniture(200,30, "Tea Table"));
+        for(Furniture f : inventory) {
+            invBar.addItem(f);
+        }
 
 
 
@@ -199,8 +206,8 @@ public class RoomScreen extends LHXScreen {
             tab.setText(">");
         }
 
-        public void addItem(String n) {
-            SideBarItem item = new SideBarItem(n);
+        public void addItem(Furniture f) {
+            SideBarItem item = new SideBarItem(f);
             this.list.add(item);
             this.list.row();
             length++;
@@ -208,8 +215,9 @@ public class RoomScreen extends LHXScreen {
 
         private class SideBarItem extends Table{
             private Actor bar;
-            public SideBarItem(String n) {
-                final String itemName = n;
+            public SideBarItem(Furniture f) {
+                final String itemName = f.getDesc();
+                final Furniture fObj = f;
                 Image item = new Image(new Texture(Gdx.files.internal(itemName + " Icon.png")));
                 Label desc  = new Label(itemName, Assets.SKIN);
                 desc.setFontScale(.65f);
@@ -227,6 +235,8 @@ public class RoomScreen extends LHXScreen {
                     public void clicked(InputEvent e, float x, float y) {
                         //remove self and if was top item, removes the bar from its successor
                         list.removeActor(itemRef);
+                        inventory.remove(fObj);
+                        Gdx.app.log("inventory",inventory.toString());
                         Array<Cell> cells = list.getCells();
                         ArrayList<Cell> cellList = new ArrayList<Cell>();
                         for(Cell c : cells) {
@@ -238,7 +248,8 @@ public class RoomScreen extends LHXScreen {
                             ((SideBar.SideBarItem) (cellList.get(0).getActor())).removeBar();
 
                         //add furniture to scrren
-                        stage.addActor(new Image(new Texture(Gdx.files.internal(itemName+".png"))));
+                        stage.addActor(fObj);
+                        length--;
 
 
                     }
@@ -248,6 +259,78 @@ public class RoomScreen extends LHXScreen {
             public void removeBar() {
                 if(bar != null)
                     removeActor(bar);
+            }
+        }
+
+
+    }
+
+    private class Furniture extends Table {
+        private float x;
+        private float y;
+        private String desc;
+        private boolean optsToggled;
+        private TextButton xBut;
+
+        public Furniture(float ix, float iy, String desc) {
+            final Furniture fRef = this;
+            this.x = ix;
+            this.y = iy;
+            this.desc = desc;
+            setPosition(x,y);
+            Image item = new Image(new Texture(Gdx.files.internal(desc + ".png")));
+            setSize(170,150);
+            xBut = new TextButton("X", Assets.SKIN);
+            xBut.setWidth(35);
+            xBut.addListener(new ClickListener() {
+                public void clicked(InputEvent e, float x, float y) {
+                    remove();
+                    inventory.add(fRef);
+                    invBar.addItem(fRef);
+
+                }
+            });
+            add(item);
+            row();
+            addListener(new DragListener(){
+                public void drag(InputEvent event, float x, float y, int pointer) {
+                    if(optsToggled) {
+                        float deltaY = y - getHeight() / 2;
+                        float deltaX = x - getWidth() / 2;
+                        if (getY() + deltaY <= 60 && getY() + deltaY >= 30 && getX() + deltaX <= 470 && getX() + deltaX >= 0) {
+                            moveBy(deltaX, deltaY);
+                        }
+                    }
+                }
+
+                @Override
+                public void dragStop(InputEvent event, float x, float y, int pointer) {
+                    toggleOpts();
+                }
+            });
+
+            addListener(new ClickListener() {
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    toggleOpts();
+                }
+
+            });
+
+            top();
+            center();
+        }
+
+        public String getDesc() {
+            return this.desc;
+        }
+
+        public void toggleOpts() {
+            optsToggled = !optsToggled;
+            if(optsToggled) {
+                addActor(xBut);
+            } else {
+                removeActor(xBut);
             }
         }
 
