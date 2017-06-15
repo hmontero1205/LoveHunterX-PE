0e8cae9cf567039af32b047b255d8431d056d94c
kevtehpro35 <kev.tehpro35@gmail.com>
2017-06-14 22:39:04 -0400

    Added winning prize for tic tac toe

--- core/src/com/lovehunterx/networking/listeners/GameEndListener.java
+++ core/src/com/lovehunterx/networking/listeners/GameEndListener.java
@@ -15,6 +15,8 @@ public class GameEndListener implements Listener {
 
         if (packet.getData("result").equals("dc")) {
             LoveHunterX.getCurrentScreen().displayNotification(packet.getData("opp") + " left the room!");
+        } else if (packet.getData("result").equals("won")) {
+            LoveHunterX.getCurrentScreen().displayNotification("You won against " + packet.getData("opp") + " and earned 50 dollars.");
         } else {
             LoveHunterX.getCurrentScreen().displayNotification("You " + packet.getData("result") + " against " + packet.getData("opp") + "!");
         }
